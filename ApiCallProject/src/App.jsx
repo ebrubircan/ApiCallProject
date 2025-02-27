d342704122692ee523c036b6499c7ab9e4bd092c
Ebru Bircan <ebrubircan2001@icloud.com>
2025-02-28 01:43:02 +0300

    SearchDiv oluşturulması ve css düzenlemeleri

--- ApiCallProject/src/App.jsx
+++ ApiCallProject/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import SearchHeader from './SearchHeader'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-
-    </>
-  )
+  return <div className='App'>
+    <SearchHeader />
+  </div>
 }
 
 export default App
---
+++ ApiCallProject/src/SearchHeader.jsx
@@ -0,0 +1,12 @@
+function SearchHeader() {
+    return ( 
+    <div className="searchDiv">
+        <form>
+            <label>Ne Arıyorsunuz?</label>
+            <input/>
+        </form>
+    </div> 
+    );
+}
+
+export default SearchHeader;
